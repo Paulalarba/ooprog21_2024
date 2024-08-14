c92853002092def8cc331ef2fb9489d58c464070
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2024-08-14 21:56:24 +0800

    Update MovieQuoteInfor.java

--- Chapter 1/MovieQuoteInfor.java
+++ Chapter 1/MovieQuoteInfor.java
@@ -1,6 +1,10 @@
 public class MovieQuoteInfor{
   public static void main(String[] args) {
-    System.out.println(""It's only after we've lost everything that we're free to do anything." —Tyler Durden" october 15, 1999);
+    
+    System.out.println("It's only after we've lost everything that we're free to do anything.");
+    System.out.println("Movie: Fight club");
+    System.out.println("tyler durden");
+    System.out.println("October 15, 1999");
+    
   }
-  
 }
