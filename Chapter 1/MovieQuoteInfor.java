c8e7fbe9204f89a26f60de6d71cadb17c758308d
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2024-08-14 21:37:37 +0800

    Create MovieQuoteInfor.java

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,6 @@
+public class MovieQuoteInfor{
+  public static void main(String[] args) {
+    System.out.println(""It's only after we've lost everything that we're free to do anything." —Tyler Durden" october 15, 1999);
+  }
+  
+}
