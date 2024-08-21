9b788a29a3aa877182366eb837bae106e4f1c397
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2024-08-21 15:12:46 +0800

    Create SwingAlert.java

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane; // import library
+
+public class SwingAlert{
+   public static void main(String[] args){
+   
+      JOptionPane.showMessageDialog(null, "HELLO WORLD!");
+   
+   }
+}
