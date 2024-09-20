d518bf95d9ab2c086f36e0c71347ab9148992ddd
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2024-09-20 23:31:16 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,22 @@
+public class FirstAddressExcercise{
+   
+   public static void showAdress(){
+      System.out.println("Huawei Campus, Bantian, Longgang District");
+   }
+   
+   public static void showCompanyName(){
+      System.out.println("Huawei Technologies Co., Ltd.");
+   } 
+   
+   public static void showPostalCode(){
+      System.out.println("518129");
+   }
+   
+   public static void main(String[] args){
+      showCompanyName();
+      showAdress();
+      showPostalCode();
+      
+      }
+
+}
