3e3ea60529e4ba6a87372ae41f97099a9f0a2fc6
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2024-10-10 23:27:31 +0800

    Create TestBloodData.java

--- Chapter 4/TestBloodData.java
+++ Chapter 4/TestBloodData.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class TestBloodData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BloodData bloodData = new BloodData();
+
+
+        System.out.println("Enter Blood Type: ");
+        String bloodType = scanner.nextLine();
+
+        System.out.println("Enter Rh Factor: ");
+        String rhFactor = scanner.nextLine();
+
+
+        bloodData.setBloodType(bloodType);
+        bloodData.setRhFactor(rhFactor);
+        bloodData.displayBloodInfo();
+    }
+}
 
